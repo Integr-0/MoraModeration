f3c0c03ede01b9241538a38dccfb1584ebff43b6
Erik Reitbauer <74710895+Reitbauer-Erik@users.noreply.github.com>
2023-01-13 20:56:06 +0100

    Initial commit

---
+++ src/main/java/erikr/mora/Mora.java
@@ -0,0 +1,19 @@
+package erikr.mora;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mora extends JavaPlugin {
+
+    public FileConfiguration config = getConfig();
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
